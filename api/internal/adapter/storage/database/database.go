ca6c2fb109649f8f30dbf31d1a4ccad0fec2c1f1
Richard James <richjames11@gmail.com>
2026-05-27 21:12:40 +0100

    fix: add PhotoAnalysis to AutoMigrate
    The photo_analysis table was not being created because the domain model
    was not registered in the AutoMigrate call.

--- api/internal/adapter/storage/database/database.go
+++ api/internal/adapter/storage/database/database.go
@@ -60,7 +60,7 @@ func (dbEnv *Env) PerformDbSetup() {
 	}
 
 	// Migrate the schema
-	err := dbEnv.Db.AutoMigrate(&domain.Album{}, &domain.Photo{}, &domain.PhotoTag{}, &domain.Setting{}, &domain.Job{}, &domain.User{}, &domain.SharedLink{})
+	err := dbEnv.Db.AutoMigrate(&domain.Album{}, &domain.Photo{}, &domain.PhotoTag{}, &domain.PhotoAnalysis{}, &domain.Setting{}, &domain.Job{}, &domain.User{}, &domain.SharedLink{})
 	if err != nil {
 		slog.Error("Failed to perform database migration", "error", err)
 		os.Exit(1)
